9bee67c3b2a64d6714a179ecd73009fe807afe5b
yasodha rani <yashodarani2022014@gmail.com>
2026-05-28 21:08:16 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div>
       <h1>College Discovery Platform</h1>
-      <p>Project working successfully</p>
+      <p>Website Working Successfully 🚀</p>
     </div>
   );
 }
